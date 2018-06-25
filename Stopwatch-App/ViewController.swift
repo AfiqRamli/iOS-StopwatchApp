2e3df1a662c44c6540e3feb38fa4c3f45eab7d74
Afiq Ramli <afiqramli@afiqs-mbp.local>
2018-06-25 12:42:30 +0800

    Initial Commit

---
+++ Stopwatch-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stopwatch-App
+//
+//  Created by Afiq Ramli on 25/06/2018.
+//  Copyright © 2018 Afiq Ramli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
